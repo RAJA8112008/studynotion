fd4f42869b2def61b1c92c1cc107df4d263024cd
Raja <kraj9380286@gmail.com>
2026-07-02 08:49:21 +0530

    Email resolve

--- StudyNotion-main/server/utils/mailSender.js
+++ StudyNotion-main/server/utils/mailSender.js
@@ -2,15 +2,13 @@ import nodemailer from "nodemailer";
 
 const mailSender = async (email, title, body) => {
     try {
-        const transporter = nodemailer.createTransport({
-            host: process.env.MAIL_HOST,
-            port: 587,
-            secure: false,
-            auth: {
-                user: process.env.MAIL_USER,
-                pass: process.env.MAIL_PASS,
-            },
-        });
+       const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
+  },
+});
 
         const info = await transporter.sendMail({
             from: `"StudyNotion" <${process.env.MAIL_USER}>`,
